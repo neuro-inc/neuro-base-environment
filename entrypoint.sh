a2ba38d1850354509cfe94ac6d00c6d8441b8ad3
Alexey Naiden <alexey.nayden@gmail.com>
2019-09-09 18:50:17 +0300

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 : "${PLATFORMAPI_SERVICE_HOST:?The image should only run in Neuromation platform due to insecure SSH configuration. Set environment variable PLATFORMAPI_SERVICE_HOST to any non-empty value to override this check.}"
+
 ldconfig
+
 /usr/sbin/sshd -De &
-make $1
+
+$1
