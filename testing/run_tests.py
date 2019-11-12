07762b25578a45a221fff0856da01eee0274e392
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-11-12 22:00:56 +0300

    fix Serhiy's comment

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -1,5 +1,6 @@
 import shlex
 import subprocess
+import sys
 from datetime import datetime
 from pathlib import Path
 from typing import Dict, Iterator, List, Set
@@ -112,7 +113,8 @@ def run_tests(commands: List[str], ignore_commands: Set[str]) -> None:
             print(f"Failed tests:")
             for fail in failed:
                 print(f"  {fail}")
-            raise RuntimeError(f"{len(failed)} tests failed")
+            print(f"{len(failed)} tests failed")
+            sys.exit(1)
 
 
 if __name__ == "__main__":
