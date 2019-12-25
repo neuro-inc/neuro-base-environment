93daf2e7da30e09a5655b89253fbbb8089428940
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-12-25 18:25:33 +0300

    review

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+{
+
 if [ ! -z "$EXPOSE_SSH" ]; then
   echo "Starting SSH server"
   /usr/sbin/sshd -e
@@ -12,11 +14,11 @@ if [ "$GCP_SERVICE_ACCOUNT_KEY_PATH" ]; then
 fi
 
 if [ -f "$NM_WANDB_TOKEN_PATH" ]; then
-  wandb login $(cat "$NM_WANDB_TOKEN_PATH")
+  wandb login "$(cat $NM_WANDB_TOKEN_PATH)"
 fi
 
-# Create named pipe for stdout+stderr
-mkfifo /tmp/output
 
-exec timeout $JOB_TIMEOUT "$@" 2>&1 | tee /tmp/output
+exec timeout $JOB_TIMEOUT "$@"
+
+} 2>&1 | tee $OUTPUT_PIPE
 exit "${PIPESTATUS[0]}"
