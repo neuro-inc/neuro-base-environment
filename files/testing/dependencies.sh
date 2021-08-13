ef96c3ad0a2693ef184ec12a4aa2ef30a6f6ff26
Yevhenii Semendiak <semendyak@gmail.com>
2021-08-13 16:33:53 +0300

    add cv2, add tests to python packages

--- files/testing/dependencies.sh
+++ files/testing/dependencies.sh
@@ -28,9 +28,23 @@ tqdm --version
 
 service cron status
 
-wandb --version
-
 neuro --version
 neuro-extras --version
 neuro-flow --version
 neuro config show
+
+# sanity check of python libs
+python -c "import scipy as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import pandas as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import cloudpickle as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import sklearn as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import matplotlib as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import PIL as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import jupyterlab as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import tqdm as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import awscli as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import google.cloud.storage as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import tensorboardX as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import cv2 as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import torchvision as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import torchaudio as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
