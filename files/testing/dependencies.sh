83552ccdf03b762125cc45c6f0de447155bbc870
costefan <konstantine.ovchynnikov@apolo.us>
2024-11-27 12:59:28 +0000

    join framework specific deps together in script

--- files/testing/dependencies.sh
+++ files/testing/dependencies.sh
@@ -45,32 +45,30 @@ python -c "import tqdm as pkg; print(f'{pkg.__package__} version: {pkg.__version
 python -c "import cv2 as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
 
 # environment specific dependencies check
-source /opt/conda/bin/activate tf
-python -c "import tensorflow as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
-source /opt/conda/bin/deactivate
-source /opt/conda/bin/activate torch
-python -c "import torchvision as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
-python -c "import torchaudio as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
-source /opt/conda/bin/deactivate
+
 pip check -v
 ### Framework-specific tests
 # test gpu availability in DL frameworks, activating conda envs
-
-
 source /opt/conda/bin/activate tf
+tensorboard --version
+python -c "import tensorboardX as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import tensorflow as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
 python gpu_tensorflow.py
 
 # execute TF beginners notebook
-wget -q https://raw.githubusercontent.com/tensorflow/docs/refs/heads/master/site/en/tutorials/quickstart/beginner.ipynb -O beginner.ipynb
+wget -q https://storage.googleapis.com/tensorflow_docs/docs/site/en/tutorials/quickstart/beginner.ipynb -O beginner.ipynb
 jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=600 beginner.ipynb
 rm beginner.ipynb
 source /opt/conda/bin/deactivate
 
 source /opt/conda/bin/activate torch
+python -c "import torchvision as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
+python -c "import torchaudio as pkg; print(f'{pkg.__package__} version: {pkg.__version__}')"
 python gpu_pytorch.py
 
 # execute Pytorch quickstart notebook
 wget -q https://pytorch.org/tutorials/_downloads/c30c1dcf2bc20119bcda7e734ce0eb42/quickstart_tutorial.ipynb -O quickstart_tutorial.ipynb
 jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=600 quickstart_tutorial.ipynb
 rm quickstart_tutorial.ipynb
+
 source /opt/conda/bin/deactivate
